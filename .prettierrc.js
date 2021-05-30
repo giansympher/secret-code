c115d7b3e2b565e4ee53b07710401c7ca6ca22f6
giansympher <gian@symph.co>
2021-05-31 00:42:43 +0800

    test

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,8 @@
 module.exports = {
   endOfLine: 'auto',
-  semi:  true,
-  trailingComma:  'es5',
-  singleQuote:  true,
-  printWidth:  80,
-  tabWidth:  2,
+  semi: true,
+  trailingComma: 'es5',
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
 };
